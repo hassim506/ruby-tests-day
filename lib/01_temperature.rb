1868705751c0d5bcba636ae6a683d29dfd330bad
hassim506 <hassimiou061@gmail.com>
2024-05-01 00:24:23 +0000

    fork-proposed

--- lib/01_temperature.rb
+++ lib/01_temperature.rb
@@ -1,8 +1,8 @@
 # lib/temperature.rb
 
 # Convertion de la temperature en celsius
-def ftoc(fahrenheit)
-  (fahrenheit - 32) * 5.0 / 9.0
+def ftoc(temperature)
+  (temperature - 32) * 5.0 / 9.0
 end
 
 # Convertion de temperature en celsius
